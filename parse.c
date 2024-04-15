bf72920a37b87ebe58b2d63becd3c3d3baf0a656
niranois13 <8784@holbertonstudents.com>
2024-04-15 19:43:42 +0200

    added the parsing of input strings. We can printf stuff passed as input

---
+++ parse.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+char **parse(char *input)
+{
+	char **tokens = NULL, *tok;
+	int position = 0;
+	const char *delim = " \t\n";
+
+	if (input == NULL)
+		return (NULL);
+
+	tokens = malloc(sizeof(char *));
+	if (tokens == NULL)
+		return (NULL);
+
+	tok = strtok(input, delim);
+	while (tok != NULL)
+	{
+		tokens = realloc(tokens, (position + 1) * sizeof(char *));
+		if (tokens == NULL)
+		{
+			for (int i = 0; i < position; i++)
+			{
+				free(tokens[i]);
+			}
+			free(tokens);
+			return (NULL);
+		}
+		tokens[position] = strdup(tok);
+		if (tokens[position] == NULL)
+		{
+			for (int i = 0; i < position; i++)
+			{
+				free(tokens[i]);
+			}
+			free(tokens);
+			return (NULL);
+		}
+		tok = strtok(NULL, delim);
+		position++;
+	}
+	tokens[position] = NULL;
+	return (tokens);
+}
